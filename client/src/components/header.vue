b16621f360cc4621d375008986f09cebe3ad95e6
AlphaaBeyy <j3n34x@hotmail.com>
2021-12-04 17:41:06 +0300

    V2

--- client/src/components/header.vue
+++ client/src/components/header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="header">
-    <a href="https://github.com/m1k1o/neko" title="Github repository" target="_blank" class="neko">
-      <img src="@/assets/images/logo.svg" alt="n.eko" />
-      <span><b>n</b>.eko</span>
+    <a href="https://dizilol.xyz" title="dizilol" target="_blank" class="neko">
+      <img src="@/assets/images/logo.svg" alt="dizilol" />
+      <span><b>WO</b>Sinema</span>
     </a>
     <ul class="menu">
       <li>
